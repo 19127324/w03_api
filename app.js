e1af426119bbbe4b3a27dfb26a746b9e76481ed8
19127324 <64062447+19127324@users.noreply.github.com>
2022-10-29 14:11:58 +0700

    update

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ var usersRouter = require('./components/users');
 
 var app = express();
 const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+const corsOptions = {
+  origin: '*',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
 // view engine setup
